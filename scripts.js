af0eb02e20c1d9f506e79d873f7fae89d5a8497f
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-17 08:18:52 -0300

    Formatando a moeda

--- scripts.js
+++ scripts.js
@@ -3,6 +3,23 @@ const amount = document.querySelector("#amount");
 
 // Evento para capturar quando o conteúdo mudar.
 amount.oninput = () => {
-  let value = amount.value.replace(/\D/g, "") // Aceitando somente números
-  amount.value = value // Repassando para o amount pra impedir de aparecer na tela
+  // Obtém o valor atual do input e remove os caracteres não numéricos.
+  let value = amount.value.replace(/\D/g, "");
+
+  // Transforma o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50).
+  value = Number(value) / 100;
+
+  // Repassando para o amount pra impedir de aparecer na tela.
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL.
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  // Retorna o valor formatado
+  return value;
+}
