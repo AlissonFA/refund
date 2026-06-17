e366c811910bb718e83412ff80ec63266d5f1e67
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-17 08:08:31 -0300

    Capturando o evento de input

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Seleciona os elementos do formulário.
+const amount2 = document.querySelector("#amount");
+
+// Evento para capturar quando o conteúdo mudar.
+amount.oninput = () => {
+  console.log("Olá");
+};
