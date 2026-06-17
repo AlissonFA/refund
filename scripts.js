54e1ca1d19c7562ba6a64d055d3c3de69c8debb6
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-17 08:08:55 -0300

    Aceitando somente números no valor

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 // Seleciona os elementos do formulário.
-const amount2 = document.querySelector("#amount");
+const amount = document.querySelector("#amount");
 
 // Evento para capturar quando o conteúdo mudar.
 amount.oninput = () => {
-  console.log("Olá");
+  let value = amount.value.replace(/\D/g, "") // Aceitando somente números
+  amount.value = value // Repassando para o amount pra impedir de aparecer na tela
 };
